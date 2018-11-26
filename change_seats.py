93dc5574a2a0d840776199ecc83390dbe9dd5526
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-26 17:36:34 +0900

    終わりだ

--- change_seats.py
+++ change_seats.py
@@ -23,32 +23,6 @@ while True:
     if len(list2) == 5:
         break
 
-# while True:
-#     random_member = random.choice(all_list)
-#     list3.append(random_member)
-#     all_list.remove(random_member)
-#     if len(list3) == 4:
-#         break
-
-# for user in list1:
-#     print(user,end = "  ")
-
-print(f"{list1[0]} {list1[1]} {list1[2]} {list1[3]} {list1[4]} {list1[5]}")
-print(f"{list2[0]} {list2[1]} {list2[2]} {list2[3]} {list2[4]}")
-print(f"{all_list[0]} {all_list[1]} {all_list[2]} {all_list[3]}")
-
-
-
-
-
-
-
-# $ python seat_shuffle.py
-# Table1: 横川 中川 鹿糠 美香子 大江 内田
-# Table2: 吉田 則也 中俣 川合 三村
-# Table3: 高橋 工藤 松本 杉村
-#
-# $ python seat_shuffle.py
-# Table1: 工藤 三村 中俣 高橋 則也 川合
-# Table2: 中川 美香子 大江 内田 横川
-# Table3: 鹿糠 吉田 杉村 松本
+print(f"Table1: {list1[0]} {list1[1]} {list1[2]} {list1[3]} {list1[4]} {list1[5]}")
+print(f"Table2: {list2[0]} {list2[1]} {list2[2]} {list2[3]} {list2[4]}")
+print(f"Table3: {all_list[0]} {all_list[1]} {all_list[2]} {all_list[3]}")
