3ce9dce1628bb18a5f6aceecc824874cbc8aafa4
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-26 17:15:33 +0900

    完成 まだ長ったらしい気がする

--- change_seats.py
+++ change_seats.py
@@ -5,40 +5,37 @@ with open("members_list.txt", mode="r") as f:
 
     all_list = members.split("\n")
 
-
 list1 = []
 list2 = []
 list3 = []
 
-for random_member in all_list:
+while True:
     random_member = random.choice(all_list)
     list1.append(random_member)
     all_list.remove(random_member)
     if len(list1) == 6:
         break
-for random_member in all_list:
+
+while True:
     random_member = random.choice(all_list)
     list2.append(random_member)
     all_list.remove(random_member)
     if len(list2) == 5:
         break
 
-print(all_list)
-
-for random_member in all_list:
+while True:
     random_member = random.choice(all_list)
     list3.append(random_member)
     all_list.remove(random_member)
     if len(list3) == 4:
         break
 
-print(all_list)
+# for user in list1:
+#     print(user,end = "  ")
 
-# print(f"{list1[0]} {list1[1]} {list1[2]} {list1[3]} {list1[4]} {list1[5]}")
-# print(f"{list2[0]} {list2[1]} {list2[2]} {list2[3]} {list2[4]}")
-# print(f"{list3[0]} {list3[1]} {list3[2]} {list3[3]}")
-
-print(list3)
+print(f"{list1[0]} {list1[1]} {list1[2]} {list1[3]} {list1[4]} {list1[5]}")
+print(f"{list2[0]} {list2[1]} {list2[2]} {list2[3]} {list2[4]}")
+print(f"{list3[0]} {list3[1]} {list3[2]} {list3[3]}")
 
 
 
