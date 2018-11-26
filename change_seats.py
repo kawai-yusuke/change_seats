b18824be0cae3286ac690fdaeb39de3b536fb16c
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-26 13:42:24 +0900

    あれ? actualがrandomのとき、どうやってテスト駆動すればいいんだ? ランダムだからexpectedがわからない

---
+++ change_seats.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+# $ python seat_shuffle.py
+# Table1: 横川 中川 鹿糠 美香子 大江 内田
+# Table2: 吉田 則也 中俣 川合 三村
+# Table3: 高橋 工藤 松本 杉村
+#
+# $ python seat_shuffle.py
+# Table1: 工藤 三村 中俣 高橋 則也 川合
+# Table2: 中川 美香子 大江 内田 横川
+# Table3: 鹿糠 吉田 杉村 松本
